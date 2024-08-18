e2fdba35095f140fdfccc21d4e96a20d68c7d21b
Guymateke <guymateke69@gmail.com>
2024-08-18 03:12:09 +0100

    premier commit

---
+++ project/src/app/housing-list/housing-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-housing-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './housing-list.component.html',
+  styleUrl: './housing-list.component.css'
+})
+export class HousingListComponent {
+
+}
